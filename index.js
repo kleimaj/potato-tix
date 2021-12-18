6b13b99b67f09f23d686b9c1c1c6e3a41558489e
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2021-12-18 15:23:49 -0800

    implements potatoScrape

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // https://dev.to/code_jedi/web-scraping-in-nodejs-2lkf
 const puppeteer = require('puppeteer');
 
+// https://www.thebakedpotato.com/events-calendar/
+// https://www.thesaurus.com/browse/smart
+
 async function scrape(){
     // Initialize Browser instance
     const browser = await puppeteer.launch({});
@@ -17,5 +20,22 @@ async function scrape(){
     console.log(text)
     // Close Browser instance
     browser.close()
+}
+async function potatoScrape() {
+        // Initialize Browser instance
+        const browser = await puppeteer.launch({});
+        // Initialize Page variable
+        const page = await browser.newPage();
+    
+        // Navigate to page
+        await page.goto('https://www.thebakedpotato.com/events-calendar/');
+        // Grab selector of first element
+        let element = await page.waitForSelector("#event-1327 > div > h1");
+        // Extract text
+        var text = await page.evaluate(element => element.textContent, element)
+        // Log Text
+        console.log(text)
+        // Close Browser instance
+        browser.close()
 }
 scrape();
