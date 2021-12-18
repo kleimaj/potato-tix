008c8f5fc1b79e77daa1f8656958740e288e5a6d
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2021-12-18 15:19:14 -0800

    implements first thesaurus test

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 const puppeteer = require('puppeteer');
 
 async function scrape(){
+    // Initialize Browser instance
+    const browser = await puppeteer.launch({});
+    // Initialize Page variable
+    const page = await browser.newPage();
 
+    // Navigate to page
+    await page.goto('https://www.thesaurus.com/browse/smart');
+    // Grab selector of first element
+    let element = await page.waitForSelector("#meanings > div.css-ixatld.e15rdun50 > ul > li:nth-child(1) > a")
+    // Extract text
+    var text = await page.evaluate(element => element.textContent, element)
+    // Log Text
+    console.log(text)
+    // Close Browser instance
+    browser.close()
 }
 scrape();
