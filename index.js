8180f64181b2237cbbc89262415cdad0c7df59c5
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2021-12-21 11:50:54 -0800

    grabs next 3 shows

--- index.js
+++ index.js
@@ -15,74 +15,92 @@ async function scrape() {
         await page.goto('https://www.thebakedpotato.com/events-calendar/');
         // Grab selector of first element
         // let element = await page.waitForSelector("#event-1327 > div > h1");
-        let element = await page.waitForSelector(".event > div > h1");
-        let dateSelector = await page.waitForSelector('.date')
+        // let element = await page.waitForSelector(".event > div > h1");
+        // let dateSelector = await page.waitForSelector('.date')
         
-        let ticketButton = await page.waitForSelector('.event > div > a');
-        // Extract text
-        var artist = await page.evaluate(element => element.textContent, element);
-        let date = await page.evaluate(dateSelector => dateSelector.textContent, dateSelector);
-        // Log Text
-        var href = await page.evaluate(ticketButton => ticketButton.href, ticketButton);
+        // let ticketButton = await page.waitForSelector('.event > div > a');
+        // // Extract text
+        // var artist = await page.evaluate(element => element.textContent, element);
+        // let date = await page.evaluate(dateSelector => dateSelector.textContent, dateSelector);
+        // // Log Text
+        // var href = await page.evaluate(ticketButton => ticketButton.href, ticketButton);
 
-        await page.goto(href); // wait for the page to load
+        let cal = await page.evaluate(()=> {
+            let artists = Array.from(document.querySelectorAll('.event > div >h1')).map((title)=>title.textContent);
+            let dates = Array.from(document.querySelectorAll('.date')).map((date)=>date.textContent);
+            let anchors = Array.from(document.querySelectorAll('.event > .title > a')).map((anchor)=>anchor.href)
 
-        var res = await page.evaluate(()=> {
-            // get ticket group elements
-            const resultMap = {
-                artist: '',
-                date: '',
-            }
-            const PATIO_COUNT = 26;
-            const INSIDE_COUNT = 65;
-            let ticketCount = 0;
-            const elements = Array.from(document.querySelectorAll('.tribe-tickets__item'));
-            // check if sold out
-            const availablitiy = elements.map((item) => item.getAttribute('data-available'));
-            availablitiy.forEach((available, idx) => {
-                const isPatio = elements[idx].querySelector('.tribe-tickets__item__content__title').textContent.includes('Patio');
-                if (available) { // compute amount of tickets available
-                    for (let i = 0; i < 70; i++) {
-                        elements[idx].querySelector('.tribe-tickets__item__quantity__add').click();
-                    }
-                    const tixAvailable = parseInt(elements[idx].querySelector('input').value);
-                    if (isPatio) {
-                        const count = PATIO_COUNT - tixAvailable;
-                        ticketCount+= count;
-                        resultMap['patio'] = count;
-                    }
-                    else {
-                        const count = (INSIDE_COUNT - tixAvailable);
-                        ticketCount+= count;
-                        // refactor this
-                        if ('set1' in resultMap) {
-                            resultMap['set2'] = count;
-                        } else {
-                            resultMap['set1'] = count;
-                        }
-                    }
-                } else { //it's sold out, add to ticketCount
-                    if (isPatio) {
-                        ticketCount+= PATIO_COUNT;
-                        resultMap['patio'] = PATIO_COUNT;
-                    } else {
-                        ticketCount+= INSIDE_COUNT;
-                        // refactor this
-                        if ('set1' in resultMap) {
-                            resultMap['set2'] = INSIDE_COUNT;
-                        } else if (idx == 1) {
-                            resultMap['set1'] = INSIDE_COUNT;
-                        }
-                    }
+            return {artists, dates, anchors}
+        })
+        // await page.goto(cal.anchors[15]);
+        for (let i = 0; i < 3; i++) {
+
+            let artist = cal.artists[i];
+            let date = cal.dates[i];
+            let href = cal.anchors[i];
+            // console.log(cal.anchors.length, cal.artists.length, cal.dates.length)
+            await page.goto(href);
+            // await page.goto(href); // wait for the page to load
+
+            var res = await page.evaluate(()=> {
+                // get ticket group elements
+                const resultMap = {
+                    artist: '',
+                    date: '',
                 }
-            })
-            resultMap['ticketCount'] = ticketCount;
-            return resultMap;
+                const PATIO_COUNT = 26;
+                const INSIDE_COUNT = 65;
+                let ticketCount = 0;
+                const elements = Array.from(document.querySelectorAll('.tribe-tickets__item'));
+                // check if sold out
+                const availablitiy = elements.map((item) => item.getAttribute('data-available'));
+                availablitiy.forEach((available, idx) => {
+                    const isPatio = elements[idx].querySelector('.tribe-tickets__item__content__title').textContent.includes('Patio');
+                    if (available) { // compute amount of tickets available
+                        for (let i = 0; i < 70; i++) {
+                            elements[idx].querySelector('.tribe-tickets__item__quantity__add').click();
+                        }
+                        const tixAvailable = parseInt(elements[idx].querySelector('input').value);
+                        if (isPatio) {
+                            const count = PATIO_COUNT - tixAvailable;
+                            ticketCount+= count;
+                            resultMap['patio'] = count;
+                        }
+                        else {
+                            const count = (INSIDE_COUNT - tixAvailable);
+                            ticketCount+= count;
+                            // refactor this
+                            if ('set1' in resultMap) {
+                                resultMap['set2'] = count;
+                            } else {
+                                resultMap['set1'] = count;
+                            }
+                        }
+                    } else { //it's sold out, add to ticketCount
+                        if (isPatio) {
+                            ticketCount+= PATIO_COUNT;
+                            resultMap['patio'] = PATIO_COUNT;
+                        } else {
+                            ticketCount+= INSIDE_COUNT;
+                            // refactor this
+                            if ('set1' in resultMap) {
+                                resultMap['set2'] = INSIDE_COUNT;
+                            } else if (idx == 1) {
+                                resultMap['set1'] = INSIDE_COUNT;
+                            }
+                        }
+                    }
+                })
+                resultMap['ticketCount'] = ticketCount;
+                return resultMap;
 
-        });
-        res['artist'] = artist;
-        res['date'] = date;
-        console.log(res);
+            });
+            artist = artist.replaceAll('\n',' ');
+            res['artist'] = artist;
+            res['date'] = date;
+            res['href'] = href;
+            console.log(res);
+        }
         // Close Browser instance
         browser.close();
 }
